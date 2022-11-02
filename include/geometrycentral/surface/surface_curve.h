0a53a17726b9d08aa81bb013fbae7aaf48045819
Arjun Lakshmipathy <lakshmipathyarjun6@gmail.com>
2022-11-02 14:02:02 -0400

    Add surface curve class

---
+++ include/geometrycentral/surface/surface_curve.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "geometrycentral/surface/exact_geodesics.h"
+#include "geometrycentral/surface/manifold_surface_mesh.h"
+#include "geometrycentral/surface/surface_point.h"
+#include "geometrycentral/surface/trace_geodesic.h"
+#include "geometrycentral/surface/vertex_position_geometry.h"
+
+using namespace geometrycentral;
+using namespace geometrycentral::surface;
+
+class SurfaceCurve {
+
+public:
+  SurfaceCurve(ManifoldSurfaceMesh* mesh, VertexPositionGeometry* geometry, GeodesicAlgorithmExact* mmpSolver);
+
+  void createFromPoints(std::vector<SurfacePoint>& curvePoints);
+
+  void deformAngle(int pointIndex, std::complex<double> newDir);
+
+  void deformDistance(int pointIndex, double newDist);
+
+  void get(std::vector<SurfacePoint>& points);
+
+  void rotate(Vector2 newDir);
+
+  void saveStartParams();
+
+  void translate(const SurfacePoint& newStartPoint);
+
+private:
+  void computeAnglesAndDistances();
+
+  void computeInitialDirection();
+
+  std::vector<SurfacePoint> connectPointsWithGeodesic(const SurfacePoint& pt1, const SurfacePoint& pt2,
+                                                      double& distance);
+
+  Vector2 inTangentBasis(const SurfacePoint& pA, const SurfacePoint& pB, const SurfacePoint& p);
+
+  Vector2 localDir(const SurfacePoint& pt1, const SurfacePoint& pt2);
+
+  int mod(int a, int b);
+
+  Vector2 parallelTransport(const SurfacePoint& startPoint, const SurfacePoint& endpoint, double initAngle);
+
+  void recompute();
+
+  SurfacePoint m_startPoint;
+  Vector2 m_initDir;
+
+  SurfacePoint m_savedStartPoint;
+  Vector2 m_savedInitDir;
+
+  std::vector<SurfacePoint> m_points;
+  std::vector<std::complex<double>> m_angles;
+  std::vector<double> m_distances;
+
+  std::unique_ptr<ManifoldSurfaceMesh> m_mesh;
+  std::unique_ptr<VertexPositionGeometry> m_geometry;
+  std::unique_ptr<GeodesicAlgorithmExact> m_mmpSolver;
+};
---
+++ src/surface/surface_curve.cpp
@@ -0,0 +1,336 @@
+#include "geometrycentral/surface/surface_curve.h"
+
+// Public Utils
+
+SurfaceCurve::SurfaceCurve(ManifoldSurfaceMesh* mesh, VertexPositionGeometry* geometry,
+                           GeodesicAlgorithmExact* mmpSolver) {
+  m_mesh.reset(mesh);
+  m_geometry.reset(geometry);
+  m_mmpSolver.reset(mmpSolver);
+}
+
+void SurfaceCurve::createFromPoints(std::vector<SurfacePoint>& curvePoints) {
+  m_points.clear();
+
+  m_startPoint = curvePoints[0];
+
+  std::vector<SurfacePoint> denseCurve;
+
+  size_t N = curvePoints.size();
+
+  SurfacePoint pt1, pt2;
+  double dist;
+
+  for (size_t i = 0; i < N; i++) {
+    pt1 = curvePoints[i];
+    pt2 = curvePoints[mod(i + 1, N)];
+    denseCurve.push_back(pt1);
+
+    std::vector<SurfacePoint> path = connectPointsWithGeodesic(pt1, pt2, dist);
+
+    if (path.size() > 2 && i != N - 1) {
+      denseCurve.insert(denseCurve.end(), path.begin(), path.end());
+      denseCurve.pop_back(); // to avoid double counting
+    }
+  }
+
+  m_points = denseCurve;
+
+  computeInitialDirection();
+  computeAnglesAndDistances();
+}
+
+void SurfaceCurve::deformAngle(int pointIndex, std::complex<double> newDir) {
+  assert(pointIndex < m_angles.size());
+  m_angles[pointIndex] = newDir;
+  recompute();
+}
+
+void SurfaceCurve::deformDistance(int pointIndex, double newDist) {
+  assert(pointIndex < m_distances.size());
+  m_distances[pointIndex] = newDist;
+  recompute();
+}
+
+void SurfaceCurve::get(std::vector<SurfacePoint>& points) { points = m_points; }
+
+void SurfaceCurve::rotate(Vector2 newDir) {
+  m_initDir = newDir;
+  recompute();
+}
+
+void SurfaceCurve::saveStartParams() {
+  m_savedStartPoint = m_startPoint;
+  m_savedInitDir = m_initDir;
+}
+
+void SurfaceCurve::translate(const SurfacePoint& newStartPoint) {
+  assert(m_savedStartPoint != SurfacePoint());
+  assert(m_savedInitDir != Vector2());
+
+  m_initDir = parallelTransport(m_savedStartPoint, newStartPoint, m_savedInitDir.arg());
+  m_startPoint = newStartPoint;
+
+  recompute();
+}
+
+// Private Utils
+
+void SurfaceCurve::computeAnglesAndDistances() {
+
+  size_t N = m_points.size();
+
+  std::vector<std::complex<double>> angles(N);
+  std::vector<double> distances(N);
+
+  SurfacePoint currNode, prevNode, nextNode;
+  std::complex<double> origDir, offsetDir, adjustedDir;
+  Vector3 currPos, nextPos;
+  size_t currIdx, nextIdx, prevIdx;
+
+  for (size_t i = 0; i < N; i++) {
+    currIdx = i;
+    nextIdx = mod(currIdx + 1, N);
+    prevIdx = mod(currIdx - 1, N);
+    currNode = m_points[currIdx];
+    nextNode = m_points[nextIdx];
+    prevNode = m_points[prevIdx];
+
+    Vector3 a = currNode.interpolate(m_geometry->inputVertexPositions);
+    Vector3 b = nextNode.interpolate(m_geometry->inputVertexPositions);
+
+    distances[i] = (a - b).norm();
+
+    origDir = localDir(currNode, nextNode);
+    offsetDir = localDir(currNode, prevNode);
+
+    // CCW angle of rotation to get to next tangent vector; for convex corners, rotation is CCW (and angle is
+    // positive), CW for non-convex corners (angle is negative).
+    adjustedDir = origDir / offsetDir;
+    angles[i] = adjustedDir;
+  }
+
+  m_angles = angles;
+  m_distances = distances;
+}
+
+void SurfaceCurve::computeInitialDirection() {
+  m_geometry->requireVertexNormals();
+  m_geometry->requireVertexTangentBasis();
+
+  SurfacePoint firstPoint = m_points[0];
+  SurfacePoint secondPoint = m_points[1];
+
+  m_initDir = localDir(firstPoint, secondPoint);
+  m_initDir /= m_initDir.norm();
+}
+
+std::vector<SurfacePoint> SurfaceCurve::connectPointsWithGeodesic(const SurfacePoint& pt1, const SurfacePoint& pt2,
+                                                                  double& distance) {
+
+  // If the source and target SurfacePoints already share a common face, no need to run the algorithm.
+  if (sharedFace(pt1, pt2) != Face()) {
+    VertexData<Vector3>& vertexPositions = m_geometry->vertexPositions;
+    distance = (pt1.interpolate(vertexPositions) - pt2.interpolate(vertexPositions)).norm();
+    return {pt1, pt2};
+  }
+
+  // Otherwise, use MMP to compute exact geodesic paths.
+  SurfacePoint source = pt1;
+  double max_propagation_distance = GEODESIC_INF;
+  std::vector<SurfacePoint> stop_points = {pt2};
+  m_mmpSolver->propagate(source, max_propagation_distance, stop_points);
+  distance = m_mmpSolver->getDistance(pt2);
+  std::vector<SurfacePoint> path = m_mmpSolver->traceBack(pt2); // gets path from query point to source
+  std::reverse(path.begin(), path.end());
+
+  return path;
+}
+
+Vector2 SurfaceCurve::inTangentBasis(const SurfacePoint& pA, const SurfacePoint& pB, const SurfacePoint& p) {
+
+  Vector2 vecP;
+  Vector3 posA = pA.interpolate(m_geometry->vertexPositions);
+  Vector3 posB = pB.interpolate(m_geometry->vertexPositions);
+  Vector3 vec = posB - posA;
+
+  switch (p.type) {
+  case (SurfacePointType::Vertex): {
+    Vertex vP = p.vertex;
+    Vector3 basisX = m_geometry->vertexTangentBasis[vP][0];
+    Vector3 basisY = m_geometry->vertexTangentBasis[vP][1];
+    vecP = {dot(vec, basisX), dot(vec, basisY)};
+    break;
+  }
+  case (SurfacePointType::Edge): {
+    Edge eP = p.edge;
+    Face f = sharedFace(pA, pB); // face containing vec
+    Vector3 fN = m_geometry->faceNormals[f];
+    Vector3 basisX = m_geometry->halfedgeVector(eP.halfedge()).normalize();
+    Vector3 basisY = cross(basisX, fN);
+    vecP = {dot(vec, basisX), dot(vec, basisY)};
+    break;
+  }
+  case (SurfacePointType::Face): {
+    Face fP = p.face;
+    Vector3 basisX = m_geometry->faceTangentBasis[fP][0];
+    Vector3 basisY = m_geometry->faceTangentBasis[fP][1];
+    vecP = {dot(vec, basisX), dot(vec, basisY)};
+    break;
+  }
+  }
+  return vecP.normalize();
+}
+
+Vector2 SurfaceCurve::localDir(const SurfacePoint& pt1, const SurfacePoint& pt2) {
+
+  // Get the local basis vectors in global coordinates
+  Halfedge heDir;
+  Vector3 normal;
+  Vector3 globalDir =
+      pt2.interpolate(m_geometry->inputVertexPositions) - pt1.interpolate(m_geometry->inputVertexPositions);
+  globalDir /= globalDir.norm();
+  m_geometry->requireVertexNormals(); // GC doesn't seem to have immediate method for vertex normals
+  double eps = 1e-8;
+
+  if (pt1.type == SurfacePointType::Face) {
+    heDir = pt1.face.halfedge();
+    normal = m_geometry->faceNormal(pt1.face);
+  } else if (pt1.type == SurfacePointType::Edge) {
+    Edge eDir = pt1.edge;
+    heDir = eDir.halfedge();
+    Vector3 local_x = m_geometry->halfedgeVector(heDir).normalize();
+    // If segment lies on the edge
+    if (abs(1.0 - dot(local_x, globalDir)) < eps) {
+      return {1.0, 0.0};
+    }
+    // If segment lies on the edge
+    else if (abs(-1.0 - dot(local_x, globalDir)) < eps) {
+      return {-1.0, 0.0};
+    }
+    // If segment lies on a face, find which face it lies in, and get its normal
+    bool found = false;
+    if (pt2.type == SurfacePointType::Face) {
+      Face f = pt2.face;
+      normal = m_geometry->faceNormal(f);
+    } else if (pt2.type == SurfacePointType::Edge) {
+      for (Face f : eDir.adjacentFaces()) {
+        for (Edge e : f.adjacentEdges()) {
+          if (pt2.edge == e) {
+            normal = m_geometry->faceNormal(f);
+            found = true;
+            break;
+          }
+        }
+        if (found) break;
+      }
+    } else {
+      for (Face f : eDir.adjacentFaces()) {
+        for (Vertex v : f.adjacentVertices()) {
+          if (pt2.vertex == v) {
+            normal = m_geometry->faceNormal(f);
+            found = true;
+            break;
+          }
+        }
+        if (found) break;
+      }
+    }
+  } else {
+    heDir = pt1.vertex.halfedge();
+    normal = m_geometry->vertexNormals[pt1.vertex]; // angle-weighted normal
+  }
+
+  Vector3 local_x = m_geometry->halfedgeVector(heDir).normalize();
+  Vector3 local_y = cross(normal, local_x);
+
+  Vector2 dir = {dot(globalDir, local_x), dot(globalDir, local_y)}; // not necessarily unit
+  dir /= dir.norm();
+  return dir;
+}
+
+int SurfaceCurve::mod(int a, int b) { return (b + (a % b)) % b; }
+
+Vector2 SurfaceCurve::parallelTransport(const SurfacePoint& startPoint, const SurfacePoint& endpoint,
+                                        double initAngle) {
+  double distance;
+
+  std::vector<SurfacePoint> path = connectPointsWithGeodesic(startPoint, endpoint, distance);
+
+  m_geometry->requireTransportVectorsAlongHalfedge();
+  m_geometry->requireVertexTangentBasis();
+  m_geometry->requireFaceTangentBasis();
+  m_geometry->requireFaceNormals();
+
+  Vector2 result = Vector2::fromAngle(initAngle);
+
+  size_t nNodes = path.size();
+  for (size_t i = 0; i < nNodes - 1; i++) {
+    SurfacePoint pA = path[i];
+    SurfacePoint pB = path[i + 1];
+    assert(sharedFace(pA, pB) != Face());
+
+    if (pA.type == SurfacePointType::Face && pB.type == SurfacePointType::Face && pA.face == pB.face) continue;
+    if (pA.type == SurfacePointType::Edge && pB.type == SurfacePointType::Edge && pA.edge == pB.edge) continue;
+
+    Vector2 rot;
+    if (pA.type == SurfacePointType::Vertex && pB.type == SurfacePointType::Vertex) {
+      // Determine the halfedge between pA.vertex -> pB.vertex
+      Halfedge heAB = Halfedge();
+      for (Halfedge he : pA.vertex.outgoingHalfedges()) {
+        if (he.tipVertex() == pB.vertex) heAB = he;
+      }
+      assert(heAB != Halfedge());
+      rot = m_geometry->transportVectorsAlongHalfedge[heAB];
+    } else {
+      Vector2 vecA = inTangentBasis(pA, pB, pA);
+      Vector2 vecB = inTangentBasis(pA, pB, pB);
+      rot = vecB / vecA;
+    }
+    result = rot * result;
+  }
+
+  return result;
+}
+
+void SurfaceCurve::recompute() {
+  // Clear all existing point data
+  m_points.clear();
+
+  // Place first points
+  m_points.push_back(m_startPoint);
+
+  // Declare some variables
+  size_t N = m_angles.size();
+
+  SurfacePoint pathEndpoint;
+  std::complex<double> endingDir;
+  TraceGeodesicResult tracedGeodesic;
+  TraceOptions traceOptions;
+  traceOptions.includePath = true;
+
+  // Trace out from first to second point
+  m_initDir /= m_initDir.norm();
+  tracedGeodesic =
+      traceGeodesic(*(m_geometry), m_startPoint, Vector2::fromComplex(m_initDir * m_distances[0]), traceOptions);
+
+  pathEndpoint = tracedGeodesic.endPoint;
+  m_points.insert(m_points.end(), tracedGeodesic.pathPoints.begin() + 1, tracedGeodesic.pathPoints.end());
+  endingDir = -tracedGeodesic.endingDir;
+
+  // Trace the rest
+  for (size_t i = 1; i < N - 1; i++) {
+    // Get the corresponding direction on S2
+    endingDir /= std::abs(endingDir);
+    std::complex<double> adjustedDirS2 = endingDir * m_angles[i];
+    adjustedDirS2 /= std::abs(adjustedDirS2); // make sure direction is unit
+
+    // Trace geodesic from last point and record where it ended up
+    tracedGeodesic = traceGeodesic(*(m_geometry), m_points[m_points.size() - 1],
+                                   Vector2::fromComplex(adjustedDirS2 * m_distances[i]), traceOptions);
+
+    pathEndpoint = tracedGeodesic.endPoint;
+    m_points.insert(m_points.end(), tracedGeodesic.pathPoints.begin() + 1, tracedGeodesic.pathPoints.end());
+    endingDir = -tracedGeodesic.endingDir;
+  }
+}
